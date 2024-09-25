8c8f16622fa46ef713052d8cda908612a2618bfe
MandicM11 <mirko.mandic996@gmail.com>
2024-09-25 22:45:42 +0200

    fix keypoints on front

--- frontend/travel-app-nextjs/pages/keypoints.js
+++ frontend/travel-app-nextjs/pages/keypoints.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { getKeyPoints, getTours, addKeyPointToTour } from '../services/api'; // Prilagodi putanju ako je potrebno
 import { useSession } from 'next-auth/react';
-
+//TODO fix keypoints on front
 const KeyPointsPage = () => {
   const { data: session, status } = useSession();
   const [keyPoints, setKeyPoints] = useState([]);
