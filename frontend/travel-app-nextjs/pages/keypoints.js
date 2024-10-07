dc9bf4dc0050ab28fde5f0d5f0d8c121a958d649
MandicM11 <mirko.mandic996@gmail.com>
2024-10-07 21:03:00 +0200

    Update keypoints.js

--- frontend/travel-app-nextjs/pages/keypoints.js
+++ frontend/travel-app-nextjs/pages/keypoints.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { getKeyPoints, getTours, addKeyPointToTour } from '../services/api'; // Prilagodi putanju ako je potrebno
 import { useSession } from 'next-auth/react';
-
+//keypoints stil bugged TODO check request from backend and match it with front
 const KeyPointsPage = () => {
   const { data: session, status } = useSession();
   const [keyPoints, setKeyPoints] = useState([]);
